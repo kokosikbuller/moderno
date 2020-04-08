6fcfae33974f025474e865c1bccb704d4e62a577
kokosikbuller <kostyakrolll@gmail.com>
2020-04-08 11:56:46 +0300

    added product slider and product

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,20 @@
 $(function(){
 
+    var mixer = mixitup('.products__inner-box');
 
+
+        $(".rate-star").rateYo({
+          rating: 5,
+          starWidth: "12px",
+          readOnly: true,
+        });
+       
+        $(".product-slider__inner").slick({
+            dots: true,
+            slidesToShow: 4,
+            slidesToScroll: 4,
+            arrows: false,
+
+        }); 
 
 });
