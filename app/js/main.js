a5e6c064262eddddca117b0d42ddd6de066e0557
kokosikbuller <kostyakrolll@gmail.com>
2020-04-11 17:13:47 +0300

    added new-items page

--- app/js/main.js
+++ app/js/main.js
@@ -1,20 +1,43 @@
-$(function(){
+$(function () {
 
-    var mixer = mixitup('.products__inner-box');
+  $(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 1000,
+    from: 0,
+    to: 600,
+    prefix: "$"
+  });
+
+  $(".rate-star").rateYo({
+    rating: 5,
+    starWidth: "12px",
+    readOnly: true,
+  });
+
+  $(".product-slider__inner").slick({
+    dots: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    arrows: false,
+  });
+
+  $(".icon-th-list").on('click', function(){
+    $(".products__item").addClass('list');
+    $(".icon-th-large ").removeClass('active');
+    $(".icon-th-list ").addClass('active');
+    $(".aside__product-item .products__item").removeClass('list');
+  });
+
+  $(".icon-th-large").on('click', function(){
+    $(".products__item").removeClass('list');
+    $(".icon-th-large ").addClass('active');
+    $(".icon-th-list ").removeClass('active');
+  });
 
 
-        $(".rate-star").rateYo({
-          rating: 5,
-          starWidth: "12px",
-          readOnly: true,
-        });
-       
-        $(".product-slider__inner").slick({
-            dots: true,
-            slidesToShow: 4,
-            slidesToScroll: 4,
-            arrows: false,
 
-        }); 
 
+
+  var mixer = mixitup('.products__inner-box');
 });
